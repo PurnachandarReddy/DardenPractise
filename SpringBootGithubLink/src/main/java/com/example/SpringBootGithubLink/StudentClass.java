2def412b11fd879878fc6ce890db443d95f4cad1
kyreddy <kyreddy@LIN-911Q6Q3.corp.capgemini.com>
2023-11-22 15:21:53 +0530

    second

--- SpringBootGithubLink/src/main/java/com/example/SpringBootGithubLink/StudentClass.java
+++ SpringBootGithubLink/src/main/java/com/example/SpringBootGithubLink/StudentClass.java
@@ -1,5 +1,33 @@
 package com.example.SpringBootGithubLink;
 
 public class StudentClass {
+	private String name;
+	private Integer age;
+	private double salary;
+	private String getName() {
+		return name;
+	}
+	private void setName(String name) {
+		this.name = name;
+	}
+	private Integer getAge() {
+		return age;
+	}
+	private void setAge(Integer age) {
+		this.age = age;
+	}
+	private double getSalary() {
+		return salary;
+	}
+	private void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public StudentClass(String name, Integer age, double salary) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
 
+	
 }
