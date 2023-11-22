8a343f3fa0797bde2dec361ea5319d46aca203bd
kyreddy <kyreddy@LIN-911Q6Q3.corp.capgemini.com>
2023-11-22 14:43:04 +0530

    First commit

---
+++ SpringBootGithubLink/src/main/java/com/example/SpringBootGithubLink/SpringBootGithubLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootGithubLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGithubLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGithubLinkApplication.class, args);
+	}
+
+}
---
+++ SpringBootGithubLink/src/test/java/com/example/SpringBootGithubLink/SpringBootGithubLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootGithubLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGithubLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
